b3d772e42eb9842d6aa77b930488d5a5357c700b
Norbi5930 <145148532+Norbi5930@users.noreply.github.com>
2024-02-25 21:53:25 +0100

    first commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from Web import app 
 
-SECRET_KEY = "adsas154a564a218ghh8fth4f"
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/handbook"
-app.config["UPLOAD_FOLDER"] = "Web/static/images/"
+SECRET_KEY = "KEY
+SQLALCHEMY_DATABASE_URI = "DATABASE"
+app.config["UPLOAD_FOLDER"] = "Web/static/images"
