9d8ca2fe6e5bf52254592a0d6e39ce1aff5f7b6e
Norbi5930 <bnorbert0925@gmail.com>
2024-02-26 21:40:40 +0100

    remove current object function

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from Web import app 
 
-SECRET_KEY = "KEY"
-SQLALCHEMY_DATABASE_URI = "DATABASE"
+SECRET_KEY = "154sadasdztdfgbvcnb456"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/handbook"
 app.config["UPLOAD_FOLDER"] = "Web/static/images"
