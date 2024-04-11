c30b5937c8cbbd5ee4c54360bf97fb01d3fe0f19
Norbi5930 <bnorbert0925@gmail.com>
2024-04-11 22:04:20 +0200

    bug fix

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from Web import app 
 
-SECRET_KEY = "KEY"
-SQLALCHEMY_DATABASE_URI = "DATABASE"
+SECRET_KEY = "asdkognings15xb15436698bcvaf"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/handbook"
 app.config["UPLOAD_FOLDER"] = "Web/static/images"
 app.config["PROFILE_PICTURE_UPLOAD_FOLDER"] = "Web/static/profile_images"
 app.config["POST_PICTURE_UPLOAD_FOLDER"] = "Web/static/posts_images"
