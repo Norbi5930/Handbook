7e36fdb1c7278226f2f6b3da127bca75b050d094
Norbi5930 <145148532+Norbi5930@users.noreply.github.com>
2024-04-11 21:57:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from Web import app 
 
-SECRET_KEY = "KEY"
-SQLALCHEMY_DATABASE_URI = "DATABASE"
+SECRET_KEY = "YOUR_KEY"
+SQLALCHEMY_DATABASE_URI = "YOUR_DATABASE"
 app.config["UPLOAD_FOLDER"] = "Web/static/images"
 app.config["PROFILE_PICTURE_UPLOAD_FOLDER"] = "Web/static/profile_images"
 app.config["POST_PICTURE_UPLOAD_FOLDER"] = "Web/static/posts_images"
